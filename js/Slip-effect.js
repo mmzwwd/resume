9020c89b50a43f2d26ca701193048358c3bc9e62
wwd <exercise666@163.comm>
2019-06-05 15:43:18 +0800

    增加鼠标滑过星星效果

---
+++ js/Slip-effect.js
@@ -0,0 +1,21 @@
+var H = 0;
+
+// $('.button').bind('mousedown touchstart',function(e) {
+//   $('.clicked').removeClass('clicked');
+//   $(this).addClass('clicked');
+// });
+
+$(document).bind('mousemove touchmove', function (e) {
+    e.preventDefault();
+    var drawSize = 10;
+    var drawType = '✭';
+    // ✭
+    var floatType = 'floatOne';
+    var xPos = e.originalEvent.pageX;
+    var yPos = e.originalEvent.pageY;
+    $('body').append('<div class="draw" style="font-size:' + drawSize + 'px;left:' + xPos + 'px;top:' + yPos + 'px;-webkit-animation:' + floatType + ' .9s 1;-moz-animation:' + floatType + ' .9s 1;color:#FFFF00;">' + drawType + '</div>');
+    $('.draw').each(function () {
+        var div = $(this);
+        setTimeout(function () { $(div).remove(); }, 800);
+    });
+});
